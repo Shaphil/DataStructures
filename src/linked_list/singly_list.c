233c5fc0edf25b0b58c2d45e7386a733c2321979
Shaphil <shaphil2025@gmail.com>
2018-04-15 20:16:44 +0600

    Linked List: Add basic Singly List
    Operations added
        * Node* init(data)
        * Node* append(head, data)
        * Node* prepend(head, data)
        * void print(head)
        * void print_reverse(head)
        * Node* free_head(head)

---
+++ src/linked_list/singly_list.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node Node;
+struct _node
+{
+    int data;
+    Node *next;
+};
+
+Node* init(int data)
+{
+    Node *node = malloc(sizeof(*node));
+    node->data = data;
+    node->next = NULL;
+
+    return node;
+}
+
+Node* append(Node *head, int data)
+{
+    // create a new node with given data
+    Node *node = init(data);
+
+    // if head is empty, make the head point to the new node
+    if (!head)
+        head = node;
+    else {
+        // there is one or more node(s) in the list
+        // iterate till the last node is found and
+        // then add after it.
+        Node *current = head;
+
+        // this loop will break if current->next is NULL
+        while (current->next)
+            current = current->next;
+
+        current->next = node;
+    }
+
+    // return updated list
+    return head;
+}
+
+Node* prepend(Node *head, int data)
+{
+    Node *node = init(data);
+
+    // add this new node to the beginning of the list
+    // by making the current head of the list its next node
+    node->next = head;
+
+    // update the head and bring it back to its glory
+    head = node;
+
+    // return updated list
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *current = head;
+    // you go beyond the last node because you need to print its data
+    while (current) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void print_reverse(Node *head)
+{
+    Node *current = head;
+    Node *reversed = NULL;
+
+    // we create a new list and prepend each node of the current list to it
+    // in that way we build a reversed list of the current list
+    while (current) {
+        reversed = prepend(reversed, current->data);
+        current = current->next;
+    }
+
+    // and then simply print the reversed list
+    print(reversed);
+}
+
+Node* free_head(Node *head)
+{
+    free(head);
+    return NULL;
+}
+
+int main()
+{
+    Node *head = NULL;
+
+    for (int i = 0; i < 10; i++)
+        head = append(head, i + 1);
+
+    print(head);
+    print_reverse(head);
+    head = free_head(head);
+
+    for (int i = 0; i < 10; i++)
+        head = prepend(head, i + 1);
+
+    print(head);
+    head = free_head(head);
+
+    return 0;
+}
