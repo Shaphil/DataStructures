784aaf9f517ee26d12b84fb16e8477ce91b0e141
Shaphil <shaphil2025@gmail.com>
2018-04-22 16:15:28 +0600

    Complete Singly List
    * Add methods for removing nodes
      - Node* remove_first(Node *head)
      - Node* remove_last(Node *head)
      - Node* remove_from(Node *head, int position)
    
    * Add Tests
      - void _log(char *name);
      - void hr();
      - Node* append_n_nodes(Node *head, int count);
      - Node* prepend_n_nodes(Node *head, int count);
      - Node* append_n_nodes_and_print(Node *head, int count, int print_length);
      - Node* prepend_n_nodes_and_print(Node *head, int count, int print_length);
      - void print_list(Node *head, char *name, int print_length);
      - void append_test(Node *head);
      - void reverse_list_test(Node *head);
      - void insert_test(Node *head);
      - void prepend_test(Node *head);
      - void update_test(Node *head);
      - void remove_first_test(Node *head);
      - void remove_last_test(Node *head);
      - void remove_from_test(Node *head);
    
    * TODO:
      - Separate the list operations.
      - Separate the Tests.
    
    ^_^

--- src/linked_list/singly_list.c
+++ src/linked_list/singly_list.c
@@ -14,49 +14,197 @@ Node* prepend(Node *head, int data);
 Node* insert(Node *head, int data, int position);
 Node* update(Node *head, int data, int position);
 Node* reverse(Node *head);
+Node* remove_first(Node *head);
+Node* remove_last(Node *head);
+Node* remove_from(Node *head, int position);
 int length(Node *head);
 void print(Node *head);
 void print_reverse(Node *head);
 Node* free_head(Node *head);
 
+// test functions
+void _log(char *name);
+void hr();
+Node* append_n_nodes(Node *head, int count);
+Node* prepend_n_nodes(Node *head, int count);
+Node* append_n_nodes_and_print(Node *head, int count, int print_length);
+Node* prepend_n_nodes_and_print(Node *head, int count, int print_length);
+void print_list(Node *head, char *name, int print_length);
+void append_test(Node *head);
+void reverse_list_test(Node *head);
+void insert_test(Node *head);
+void prepend_test(Node *head);
+void update_test(Node *head);
+void remove_first_test(Node *head);
+void remove_last_test(Node *head);
+void remove_from_test(Node *head);
+
+
+// TODO: Break this into smaller files.
+
 int main()
 {
     Node *head = NULL;
 
-    // append
-    for (int i = 0; i < 10; i++)
-        head = append(head, i + 1);
-
-    printf("length: %d\n", length(head));
-    print(head);
-    print_reverse(head);
-
-    // reverse
-    Node *reversed = reverse(head);
-    print(reversed);
-
-    // insert after the nth node
-    head = insert(head, 2147483647, 7);
-    printf("length: %d\n", length(head));
-    printf("insert: ");
-    print(head);
-
-    head = free_head(head);
-
-    // prepend
-    for (int i = 0; i < 10; i++)
-        head = prepend(head, i + 1);
-
-    print(head);
-
-    // update
-    head = update(head, 2147483647, 8);
-    print(head);
-
-    head = free_head(head);
+    append_test(head);
+    reverse_list_test(head);
+    insert_test(head);
+    prepend_test(head);
+    update_test(head);
+    remove_first_test(head);
+    remove_last_test(head);
+    remove_from_test(head);
 
     return 0;
 }
+
+// Begin Tests
+
+void _log(char *name)
+{
+    printf("\t\t%s Test\n", name);
+    for (int i = 0; i < 48; i++)
+        printf("-");
+    printf("\n");
+}
+
+void hr()
+{
+    for (int i = 0; i < 48; i++)
+        printf("-");
+    printf("\n\n");
+}
+
+Node* append_n_nodes(Node *head, int count)
+{
+    for (int i = 0; i < count; i++)
+        head = append(head, i + 1);
+    return head;
+}
+
+Node* prepend_n_nodes(Node *head, int count)
+{
+    for (int i = 0; i < count; i++)
+        head = prepend(head, i + 1);
+    return head;
+}
+
+Node* append_n_nodes_and_print(Node *head, int count, int print_length)
+{
+    head = append_n_nodes(head, count);
+    print_list(head, "original", print_length);
+
+    return head;
+}
+
+Node* prepend_n_nodes_and_print(Node *head, int count, int print_length)
+{
+    head = prepend_n_nodes(head, count);
+    print_list(head, "original", print_length);
+
+    return head;
+}
+
+void print_list(Node *head, char *name, int print_length)
+{
+    if (print_length)
+        printf("length: %d\n", length(head));
+
+    printf("%s list: ", name);
+    print(head);
+}
+
+void append_test(Node *head)
+{
+    _log("Append");
+
+    head = append_n_nodes_and_print(head, 10, 0);
+    printf("print reverse: ");
+    print_reverse(head);
+    hr();
+}
+
+void reverse_list_test(Node *head)
+{
+    _log("Reverse List");
+
+    head = append_n_nodes_and_print(head, 10, 0);
+
+    Node *reversed = reverse(head);
+    print_list(reversed, "reversed", 0);
+    print_list(reversed, "original", 0);
+    hr();
+}
+
+void insert_test(Node *head)
+{
+    _log("Insert");
+
+    head = append_n_nodes_and_print(head, 10, 1);
+
+    head = insert(head, 2147483647, 7);
+    print_list(head, "updated", 1);
+    hr();
+}
+
+void prepend_test(Node *head)
+{
+    _log("Prepend");
+
+    head = prepend_n_nodes_and_print(head, 10, 1);
+    hr();
+}
+
+void update_test(Node *head)
+{
+    _log("Update");
+
+    head = append_n_nodes_and_print(head, 10, 1);
+    head = update(head, 2147483647, 8);
+    print_list(head, "updated", 1);
+    hr();
+}
+
+void remove_first_test(Node *head)
+{
+    _log("Remove First");
+
+    head = append_n_nodes_and_print(head, 10, 0);
+    printf("\nRemove all nodes from the beginning\n");
+    while (head) {
+        head = remove_first(head);
+        print(head);
+    }
+    hr();
+}
+
+void remove_last_test(Node *head)
+{
+    _log("Remove Last");
+
+    head = append_n_nodes_and_print(head, 10, 0);
+    printf("\nRemove all nodes from the end\n");
+    while (head) {
+        head = remove_last(head);
+        print(head);
+    }
+    hr();
+}
+
+void remove_from_test(Node *head)
+{
+    _log("Remove From");
+
+    head = append_n_nodes_and_print(head, 10, 0);
+    printf("\nRemove all nodes from the 3rd position\n");
+    for (int i = 0; i < 10; i++) {
+        head = remove_from(head, 3);
+        print(head);
+    }
+    hr();
+}
+
+// End Tests
 
 Node* init(int data)
 {
@@ -167,6 +315,14 @@ Node* reverse(Node *head)
     return reversed;
 }
 
+// remove_first: remove a node from the top/beginning
+Node* remove_first(Node *head)
+{
+    if (head)
+        head = head->next;
+    return head;
+}
+
 int length(Node *head)
 {
     int len = 0;
@@ -175,6 +331,48 @@ int length(Node *head)
         len++;
     }
     return len;
+}
+
+// remove_last: removes the last node of the list
+Node* remove_last(Node *head)
+{
+    int len = length(head);
+
+    // there is only one node in the list
+    if (len == 1)
+        head = head->next;
+    else if (len > 1) {
+        Node *current = head;
+
+        // we want the node before the last node
+        while (current->next->next)
+            current = current->next;
+
+        current->next = NULL;
+    }
+    return head;
+}
+
+// remove_from(head, pos)
+// remove a node from the specified position if possible
+Node* remove_from(Node *head, int position)
+{
+    int len = length(head);
+
+    if (position == 0)
+        return remove_first(head);
+    else if (position == len)
+        return remove_last(head);
+    else if (position > 0 && position < len) {
+        int count = 1;
+        Node *current = head;
+        while (count < position) {
+            current = current->next;
+            count++;
+        }
+        current->next = current->next->next;
+    }
+    return head;
 }
 
 void print(Node *head)
